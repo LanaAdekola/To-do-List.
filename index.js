fd73761cb1891fc53e9e030e7841bd9b71279343
LanaAdekola <adekolaadelana@gmail.com>
2023-02-08 13:35:14 +0100

    update

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 let inputEl = document.getElementById("input-el")
-let addbtn = document.getElementById("add-btn")
+let addBtn = document.getElementById("add-btn")
 let paragraph = document.getElementById("paragraph")
+let myLeads = []
 
-addbtn.addEventListener("click" function() {
-let inputvalue = inputEl.value
-paragraph.textContent = inputvalue
+addBtn.addEventListener("click", function(){
+   myLeads.push(inputEl.value)
+  render()
+  inputEl.value = ""
 })
+function render() {
+    let skills = ""
+    for (let i = 0; i < myLeads.length; i++){
+    skills += 
+            `
+            <li>${myLeads[i]}</li>
+            `
+    }
+paragraph.innerHTML = skills
+
+}
